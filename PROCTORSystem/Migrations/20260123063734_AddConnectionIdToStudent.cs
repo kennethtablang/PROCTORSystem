db9868a82c9b3f8cc4a0fbdb5f3907c132cf4338
kei3224 <krrtablang_19ac0055@psu.edu.ph>
2026-01-30 13:57:56 +0800

    Migrations commit for databasing and relational structure

---
+++ PROCTORSystem/Migrations/20260123063734_AddConnectionIdToStudent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PROCTORSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConnectionIdToStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConnectionId",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConnectionId",
+                table: "Students");
+        }
+    }
+}
